9bd3504cc98852589973b5cb76efee1252ff98c1
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-03-31 02:11:03 +1000

    Update SpaceObject.java

--- src/game/core/SpaceObject.java
+++ src/game/core/SpaceObject.java
@@ -6,8 +6,8 @@ import game.ui.ObjectGraphic;
  * Public interface representing a space object in the game.
  */
 public interface SpaceObject {
-    public int getX();
-    public int getY();
-    public ObjectGraphic render();
-    public void tick(int tick);
+    int getX();
+    int getY();
+    ObjectGraphic render();
+    void tick(int tick);
 }
