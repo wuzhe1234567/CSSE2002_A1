e78f27521fb302be6f1130ee1f4ad54b1f28dbba
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-03-31 01:46:42 +1000

    Update ShieldPowerUp.java

--- src/game/core/ShieldPowerUp.java
+++ src/game/core/ShieldPowerUp.java
@@ -6,14 +6,8 @@ import game.ui.ObjectGraphic;
  * Represents a shield power-up that grants the ship temporary invincibility.
  */
 public class ShieldPowerUp extends PowerUp {
-    private int duration = 50; // 固定持续时间
+    private int duration = 50;
     
-    /**
-     * Constructs a ShieldPowerUp at the given coordinates.
-     *
-     * @param x the x-coordinate.
-     * @param y the y-coordinate.
-     */
     public ShieldPowerUp(int x, int y) {
         super(x, y);
     }
@@ -25,17 +19,7 @@ public class ShieldPowerUp extends PowerUp {
     
     @Override
     public void applyEffect(Ship ship) {
-        // 这里只打印日志，实际中可调用 ship.activateShield(duration) 等方法
         System.out.println("Shield activated for " + duration + " ticks.");
     }
-    
-    @Override
-    public int getX() {
-        return x;
-    }
-    
-    @Override 
-    public int getY() {
-        return y;
-    }
 }
+
