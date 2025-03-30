d6b0a5e5f392acbc78c04c5f832dca19902f8513
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-03-31 01:06:02 +1000

    Update PowerUp.java

--- src/game/core/PowerUp.java
+++ src/game/core/PowerUp.java
@@ -7,22 +7,17 @@ import game.ui.ObjectGraphic;
  */
 public abstract class PowerUp extends SpaceObject {
 
-    /**
-     * Constructs a PowerUp with the specified coordinates.
-     *
-     * @param x the x-coordinate.
-     * @param y the y-coordinate.
-     */
     public PowerUp(int x, int y) {
         super(x, y);
     }
     
     /**
      * When the power-up is collected by the ship, apply its effect.
+     * 此处要求方法名称为 applyEffect 而非 apply。
      *
      * @param ship the ship that collects the power-up.
      */
-    public abstract void apply(Ship ship);
+    public abstract void applyEffect(Ship ship);
     
     @Override
     public void tick(int tick) {
