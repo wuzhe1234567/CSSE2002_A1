8d93da13c9772eb09f5a6ebcf457fda03e6e9266
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-03-30 23:15:54 +1000

    Update ObjectWithPosition.java

--- src/game/core/ObjectWithPosition.java
+++ src/game/core/ObjectWithPosition.java
@@ -3,49 +3,24 @@ package game.core;
 import game.ui.ObjectGraphic;
 
 /**
- * Abstract class representing an object with a position.
+ * Abstract class representing a generic power-up.
  */
-public abstract class ObjectWithPosition {
-    protected int x;
-    protected int y;
+public abstract class PowerUp extends SpaceObject {
 
-    /**
-     * Constructs an object with the specified coordinates.
-     *
-     * @param x the x-coordinate.
-     * @param y the y-coordinate.
-     */
-    public ObjectWithPosition(int x, int y) {
-        this.x = x;
-        this.y = y;
+    public PowerUp(int x, int y) {
+        super(x, y);
     }
 
     /**
-     * Returns the x-coordinate.
+     * When the power-up is collected by the ship, apply its effect.
      *
-     * @return the x-coordinate.
+     * @param ship the ship that collects the power-up.
      */
-    public abstract int getX();
+    public abstract void apply(Ship ship);
 
-    /**
-     * Returns the y-coordinate.
-     *
-     * @return the y-coordinate.
-     */
-    public abstract int getY();
-
-    /**
-     * Returns the graphical representation of the object.
-     *
-     * @return an ObjectGraphic representing the object.
-     */
-    public abstract ObjectGraphic render();
-
-    /**
-     * Updates the object's state on each game tick.
-     *
-     * @param tick the current game tick.
-     */
-    public abstract void tick(int tick);
+    @Override
+    public void tick(int tick) {
+        // By default, power-ups remain stationary.
+    }
 }
 
