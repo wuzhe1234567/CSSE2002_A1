425a3b8246f6a2986382be09c3e9fcd576510855
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-03-30 22:05:06 +1000

    Update Enemy.java

--- src/game/core/Enemy.java
+++ src/game/core/Enemy.java
@@ -5,7 +5,9 @@ import game.ui.ObjectGraphic;
 /**
  * Represents an enemy object.
  */
-public class Enemy extends SpaceObject {
+public class Enemy implements SpaceObject {
+    private int x;
+    private int y;
 
     /**
      * Constructs an Enemy with the specified coordinates.
@@ -14,24 +16,37 @@ public class Enemy extends SpaceObject {
      * @param y the y-coordinate of the enemy.
      */
     public Enemy(int x, int y) {
-        super(x, y);
+        this.x = x;
+        this.y = y;
     }
 
     @Override
     public int getX() {
         return x;
     }
-    
+
     @Override
     public int getY() {
         return y;
     }
 
+    /**
+     * Returns the graphical representation of the enemy,
+     * using the image from "src/assets/enemy.png".
+     *
+     * @return an ObjectGraphic representing the enemy.
+     */
     @Override
     public ObjectGraphic render() {
         return new ObjectGraphic("Enemy", "src/assets/enemy.png");
     }
 
+    /**
+     * Updates the enemy's state on each tick.
+     * In this implementation, the enemy moves downward.
+     *
+     * @param tick the current game tick.
+     */
     @Override
     public void tick(int tick) {
         y++; // Move downward each tick
