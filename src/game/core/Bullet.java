3185e51ae090ed4b626c07ff442e1ecda1b8fd90
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-03-30 21:56:55 +1000

    Update Bullet.java

--- src/game/core/Bullet.java
+++ src/game/core/Bullet.java
@@ -21,29 +21,17 @@ public class Bullet extends SpaceObject {
     public int getX() {
         return x;
     }
-
+    
     @Override
     public int getY() {
         return y;
     }
 
-    /**
-     * Returns the graphical representation of the bullet,
-     * using the image from "src/assets/bullet.png".
-     *
-     * @return an ObjectGraphic representing the bullet.
-     */
     @Override
     public ObjectGraphic render() {
         return new ObjectGraphic("Bullet", "src/assets/bullet.png");
     }
 
-    /**
-     * Updates the bullet's state on each tick.
-     * In this implementation, the bullet moves upward.
-     *
-     * @param tick the current game tick.
-     */
     @Override
     public void tick(int tick) {
         y--; // Move upward each tick
