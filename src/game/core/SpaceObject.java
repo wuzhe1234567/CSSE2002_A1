b970150c018a38afe48723f4d2a9cef40f2e76f8
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-03-31 01:44:33 +1000

    Update SpaceObject.java

--- src/game/core/SpaceObject.java
+++ src/game/core/SpaceObject.java
@@ -3,27 +3,12 @@ package game.core;
 import game.ui.ObjectGraphic;
 
 /**
- * Abstract class representing a space object in the game.
+ * Public interface representing a space object in the game.
  */
-public abstract class SpaceObject {
-    protected int x;
-    protected int y;
-    
-    /**
-     * Constructs a space object with the given coordinates.
-     *
-     * @param x the x-coordinate.
-     * @param y the y-coordinate.
-     */
-    public SpaceObject(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-    
-    public abstract int getX();
-    public abstract int getY();
-    public abstract ObjectGraphic render();
-    public abstract void tick(int tick);
+public interface SpaceObject {
+    int getX();
+    int getY();
+    ObjectGraphic render();
+    void tick(int tick);
 }
-
 
