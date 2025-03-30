ed14da5c2c0d2ec263cac7321d2d05e1c6c63745
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-03-30 23:11:23 +1000

    Update HealthPowerUp.java

--- src/game/core/HealthPowerUp.java
+++ src/game/core/HealthPowerUp.java
@@ -1,1 +1,43 @@
+package game.core;
+
+import game.ui.ObjectGraphic;
+
+/**
+ * Represents a health power-up that increases the ship's health.
+ */
+public class HealthPowerUp extends PowerUp {
+    private int healAmount;
+    
+    /**
+     * Constructs a HealthPowerUp at the given coordinates.
+     *
+     * @param x the x-coordinate.
+     * @param y the y-coordinate.
+     * @param healAmount the amount of health to add when applied.
+     */
+    public HealthPowerUp(int x, int y, int healAmount) {
+        super(x, y);
+        this.healAmount = healAmount;
+    }
+    
+    @Override
+    public ObjectGraphic render() {
+        return new ObjectGraphic("HealthPowerUp", "src/assets/health.png");
+    }
+    
+    @Override
+    public void apply(Ship ship) {
+        ship.heal(healAmount);
+    }
+    
+    @Override
+    public int getX() {
+        return x;
+    }
+    
+    @Override
+    public int getY() {
+        return y;
+    }
+}
 
