8b15bd2ba5711c6693298544cdde7cc04dee3257
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-03-30 21:24:50 +1000

    Update Bullet.java

--- src/game/core/Bullet.java
+++ src/game/core/Bullet.java
@@ -5,9 +5,7 @@ import game.ui.ObjectGraphic;
 /**
  * Represents a bullet object.
  */
-public class Bullet implements SpaceObject {
-    private int x;
-    private int y;
+public class Bullet extends SpaceObject {
 
     /**
      * Constructs a Bullet with the specified coordinates.
@@ -16,18 +14,29 @@ public class Bullet implements SpaceObject {
      * @param y the y-coordinate of the bullet.
      */
     public Bullet(int x, int y) {
-        this.x = x;
-        this.y = y;
+        super(x, y);
     }
 
+    /**
+     * Returns the x-coordinate of the bullet.
+     *
+     * @return the x-coordinate.
+     */
     @Override
     public int getX() {
         return x;
     }
+
+    /**
+     * Returns the y-coordinate of the bullet.
+     *
+     * @return the y-coordinate.
+     */
     @Override
     public int getY() {
         return y;
     }
+
     /**
      * Returns the graphical representation of the bullet,
      * using the image from the relative path "src/assets/bullet.png".
@@ -38,15 +47,16 @@ public class Bullet implements SpaceObject {
     public ObjectGraphic render() {
         return new ObjectGraphic("Bullet", "src/assets/bullet.png");
     }
+
     /**
      * Updates the bullet's state on each tick.
-     * In this implementation, the bullet moves upward.
+     * In this implementation, the bullet moves upward (decreases its y-coordinate).
      *
      * @param tick the current game tick.
      */
     @Override
     public void tick(int tick) {
-        y--; // Move upward each tick
+        y--; // Bullet moves upward each tick
     }
 }
 
