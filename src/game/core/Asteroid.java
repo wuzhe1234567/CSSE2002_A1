3cfe6c95abdbf5ea6bf88869a4fb9dfc02c5f0c3
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-03-30 19:34:13 +1000

    Update Asteroid.java

--- src/game/core/Asteroid.java
+++ src/game/core/Asteroid.java
@@ -1,1 +1,29 @@
+package game.core;
 
+import game.ui.ObjectGraphic;
+
+/**
+ * 表示陨石对象。
+ */
+public class Asteroid extends SpaceObject {
+
+    public Asteroid(int x, int y) {
+        super(x, y);
+    }
+
+    /**
+     * 返回陨石的图形表示，使用 "asteroid.png" 作为图片资源。
+     */
+    @Override
+    public ObjectGraphic render() {
+        return new ObjectGraphic("Asteroid", "C:\\Users\\wuzhe\\Desktop\\Study\\Semester 2\\CSSE 2002\\Assignment1\\provided\\CSSE2002_A1\\assets\\asteroid.png");
+    }
+
+    /**
+     * 陨石每个 tick 向下移动（y 增加）。
+     */
+    @Override
+    public void update() {
+        y++; // 向下移动
+    }
+}
