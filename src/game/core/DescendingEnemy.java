8ba36adada65c3b46dce8d8e3630bd8a50192cfe
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-03-31 01:23:29 +1000

    Update DescendingEnemy.java

--- src/game/core/DescendingEnemy.java
+++ src/game/core/DescendingEnemy.java
@@ -3,9 +3,9 @@ package game.core;
 import game.ui.ObjectGraphic;
 
 /**
- * Represents an enemy that descends faster.
+ * Abstract class representing an enemy that descends faster.
  */
-public class DescendingEnemy extends Enemy {
+public abstract class DescendingEnemy extends Enemy {
 
     public DescendingEnemy(int x, int y) {
         super(x, y);
@@ -13,15 +13,11 @@ public class DescendingEnemy extends Enemy {
     
     @Override
     public void tick(int tick) {
-        // Descend 2 units per tick instead of 1.
-        // 由于 Enemy 已经提供了构造函数，我们重新实现 tick 以加快下降速度。
-        // 这里直接操作 y 字段：
+        // Descend faster: 2 units per tick instead of 1.
         y += 2;
     }
     
+    // 将 render 方法声明为 abstract，要求子类提供具体图像
     @Override
-    public ObjectGraphic render() {
-        // 如果有专用图像，可修改路径；否则使用同 Enemy 的图像。
-        return new ObjectGraphic("DescendingEnemy", "src/assets/enemy.png");
-    }
+    public abstract ObjectGraphic render();
 }
