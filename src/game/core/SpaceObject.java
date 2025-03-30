14bf41d162f7d5519f49376c46915a1f126515ed
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-03-30 23:14:45 +1000

    Update SpaceObject.java

--- src/game/core/SpaceObject.java
+++ src/game/core/SpaceObject.java
@@ -20,31 +20,8 @@ public abstract class SpaceObject {
         this.y = y;
     }
     
-    /**
-     * Returns the x-coordinate.
-     *
-     * @return the x-coordinate.
-     */
     public abstract int getX();
-    
-    /**
-     * Returns the y-coordinate.
-     *
-     * @return the y-coordinate.
-     */
     public abstract int getY();
-    
-    /**
-     * Returns the graphical representation of the object.
-     *
-     * @return an ObjectGraphic representing the object.
-     */
     public abstract ObjectGraphic render();
-    
-    /**
-     * Updates the object's state on each game tick.
-     *
-     * @param tick the current game tick.
-     */
     public abstract void tick(int tick);
 }
