3e6380bba46e9c32cf4610ea5d13be69af578efb
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-03-30 22:36:53 +1000

    Update Controllable.java

--- src/game/core/Controllable.java
+++ src/game/core/Controllable.java
@@ -4,28 +4,15 @@ import game.exceptions.BoundaryExceededException;
 import game.utility.Direction;
 
 /**
- * Abstract class representing a controllable game object.
- * (如果需要，Controllable 可作为接口实现，但此处用抽象类方式满足规范要求)
+ * Public abstract interface representing a controllable game object.
+ * Extends SpaceObject.
  */
-public abstract class Controllable extends Ship {
-    /**
-     * Constructs a controllable Ship.
-     *
-     * @param x the x-coordinate.
-     * @param y the y-coordinate.
-     */
-    public Controllable(int x, int y) {
-        super(x, y);
-    }
-
+public interface Controllable extends SpaceObject {
     /**
      * Moves the object in the specified direction.
-     * Default implementation throws an UnsupportedOperationException.
      *
      * @param direction the direction to move.
      * @throws BoundaryExceededException if the move exceeds game boundaries.
      */
-    public void move(game.utility.Direction direction) throws BoundaryExceededException {
-        throw new UnsupportedOperationException("move method not implemented");
-    }
+    public abstract void move(Direction direction) throws BoundaryExceededException;
 }
