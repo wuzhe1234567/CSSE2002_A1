c4bb1248654fc5259df33989cfb5ef4a2e5a9612
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-03-30 19:25:08 +1000

    Update Logger.java

--- src/game/utility/Logger.java
+++ src/game/utility/Logger.java
@@ -1,5 +1,13 @@
 package game.utility;
 
+/**
+ * Logger interface for logging messages.
+ */
 public interface Logger {
+    /**
+     * Logs the provided message.
+     *
+     * @param message the message to log
+     */
     void log(String message);
 }
