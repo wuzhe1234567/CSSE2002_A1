44bf1ffbb97d2e33515714bcd677000af044eafa
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-03-30 21:44:59 +1000

    Update Asteroid.java

--- src/game/core/Asteroid.java
+++ src/game/core/Asteroid.java
@@ -5,7 +5,14 @@ import game.ui.ObjectGraphic;
 /**
  * Represents an asteroid object.
  */
-public class Asteroid extends ObjectWithPosition {
+public class Asteroid extends SpaceObject {
+
+    /**
+     * Constructs an Asteroid with the specified coordinates.
+     *
+     * @param x the x-coordinate of the asteroid.
+     * @param y the y-coordinate of the asteroid.
+     */
     public Asteroid(int x, int y) {
         super(x, y);
     }
@@ -14,20 +21,32 @@ public class Asteroid extends ObjectWithPosition {
     public int getX() {
         return x;
     }
+
     @Override
     public int getY() {
         return y;
     }
+
+    /**
+     * Returns the graphical representation of the asteroid,
+     * using the image from the relative path "src/assets/asteroid.png".
+     *
+     * @return an ObjectGraphic representing the asteroid.
+     */
     @Override
     public ObjectGraphic render() {
         return new ObjectGraphic("Asteroid", "src/assets/asteroid.png");
     }
+
+    /**
+     * Updates the asteroid's state on each tick.
+     * In this implementation, the asteroid moves downward.
+     *
+     * @param tick the current game tick.
+     */
     @Override
     public void tick(int tick) {
         y++; // Move downward each tick
     }
 }
 
-        y++; // Move downward each tick
-    }
-}
