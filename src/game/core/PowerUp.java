b5f3031074fbd200e029bb44ab254557940c7e1c
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-03-30 23:11:53 +1000

    Update PowerUp.java

--- src/game/core/PowerUp.java
+++ src/game/core/PowerUp.java
@@ -1,1 +1,26 @@
+package game.core;
+
+import game.ui.ObjectGraphic;
+
+/**
+ * Abstract class representing a generic power-up.
+ */
+public abstract class PowerUp extends SpaceObject {
+
+    public PowerUp(int x, int y) {
+        super(x, y);
+    }
+    
+    /**
+     * When the power-up is collected by the ship, apply its effect.
+     *
+     * @param ship the ship that collects the power-up.
+     */
+    public abstract void apply(Ship ship);
+    
+    @Override
+    public void tick(int tick) {
+        // By default, power-ups remain stationary.
+    }
+}
 
