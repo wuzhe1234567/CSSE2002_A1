185f26f3cb05819db2f9bf46c41f9abf4e6fd4ee
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-03-30 20:39:34 +1000

    Update Controllable.java

--- src/game/core/Controllable.java
+++ src/game/core/Controllable.java
@@ -1,1 +1,17 @@
+package game.core;
+
+import game.exceptions.BoundaryExceededException;
+
+/**
+ * Interface for controllable game objects.
+ */
+public interface Controllable {
+    /**
+     * Moves the object in the specified direction.
+     *
+     * @param direction the direction to move
+     * @throws BoundaryExceededException if the move would exceed game boundaries
+     */
+    void move(game.utility.Direction direction) throws BoundaryExceededException;
+}
 
