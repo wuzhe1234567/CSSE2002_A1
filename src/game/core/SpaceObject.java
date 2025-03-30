148cbba2182002208489d8e92cc2c37957631194
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-03-30 22:23:43 +1000

    Update SpaceObject.java

--- src/game/core/SpaceObject.java
+++ src/game/core/SpaceObject.java
@@ -5,32 +5,10 @@ import game.ui.ObjectGraphic;
 /**
  * Public abstract interface representing a space object in the game.
  */
-public abstract interface SpaceObject {
-    /**
-     * Returns the x-coordinate of the object.
-     *
-     * @return the x-coordinate.
-     */
+public interface SpaceObject {
     public abstract int getX();
-
-    /**
-     * Returns the y-coordinate of the object.
-     *
-     * @return the y-coordinate.
-     */
     public abstract int getY();
-
-    /**
-     * Returns the graphical representation of the object.
-     *
-     * @return an ObjectGraphic representing the object.
-     */
     public abstract ObjectGraphic render();
-
-    /**
-     * Updates the object's state on each game tick.
-     *
-     * @param tick the current game tick.
-     */
     public abstract void tick(int tick);
 }
+
