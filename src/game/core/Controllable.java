d742c028674f74619f7f9837d89d841d630d359e
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-03-31 12:36:01 +1000

    Update Controllable.java

--- src/game/core/Controllable.java
+++ src/game/core/Controllable.java
@@ -8,7 +8,7 @@ import game.utility.Direction;
  * Inherits from ObjectWithPosition.
  */
 public abstract class Controllable extends ObjectWithPosition {
-
+    
     /**
      * Constructs a controllable object with the given coordinates.
      *
@@ -21,13 +21,9 @@ public abstract class Controllable extends ObjectWithPosition {
     
     /**
      * Moves the object in the specified direction.
-     * Default implementation throws an UnsupportedOperationException.
-     * Subclasses should override this method to provide specific behavior.
      *
      * @param direction the direction to move.
      * @throws BoundaryExceededException if the move exceeds game boundaries.
      */
-    public void move(Direction direction) throws BoundaryExceededException {
-        throw new UnsupportedOperationException("move method not implemented");
-    }
+    public abstract void move(Direction direction) throws BoundaryExceededException;
 }
