a8c581857c69a27d3860bbc1daf0835c0d0e90ac
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-03-31 10:53:48 +1000

    Update Controllable.java

--- src/game/core/Controllable.java
+++ src/game/core/Controllable.java
@@ -4,10 +4,26 @@ import game.exceptions.BoundaryExceededException;
 import game.utility.Direction;
 
 /**
- * Public interface representing a controllable game object.
- * Extends SpaceObject.
+ * Public abstract class representing a controllable game object.
+ * Inherits from ObjectWithPosition.
  */
-public interface Controllable extends SpaceObject {
-    void move(Direction direction) throws BoundaryExceededException;
-}
+public abstract class Controllable extends ObjectWithPosition {
 
+    /**
+     * Constructs a controllable object with the given coordinates.
+     *
+     * @param x the x-coordinate.
+     * @param y the y-coordinate.
+     */
+    public Controllable(int x, int y) {
+        super(x, y);
+    }
+    
+    /**
+     * Moves the object in the specified direction.
+     *
+     * @param direction the direction to move.
+     * @throws BoundaryExceededException if the move exceeds game boundaries.
+     */
+    public abstract void move(Direction direction) throws BoundaryExceededException;
+}
