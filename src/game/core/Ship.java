ea2ed0f0b4d8e96f671d58cc932cc46835586ed5
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-03-31 14:47:55 +1000

    Update Ship.java

--- src/game/core/Ship.java
+++ src/game/core/Ship.java
@@ -13,15 +13,15 @@ public class Ship extends Controllable {
     private int score;
     
     /**
-     * Constructs a Ship with the specified coordinates.
-     * The ship's initial health is fixed at 100 and score is 0.
+     * Constructs a Ship with the specified coordinates and initial health.
      *
      * @param x the x-coordinate of the ship.
      * @param y the y-coordinate of the ship.
+     * @param health the initial health of the ship.
      */
-    public Ship(int x, int y) {
+    public Ship(int x, int y, int health) {
         super(x, y);
-        this.health = 100;
+        this.health = health;
         this.score = 0;
     }
     
@@ -32,14 +32,14 @@ public class Ship extends Controllable {
     
     @Override
     public void tick(int tick) {
-        // Ship movement is controlled by player input; no automatic update.
+        // Ship's movement is controlled by player input; no automatic update.
     }
     
     @Override
     public void move(Direction direction) throws BoundaryExceededException {
         int newX = x;
         int newY = y;
-        switch (direction) {
+        switch(direction) {
             case UP:
                 newY = y - 1;
                 break;
@@ -61,6 +61,7 @@ public class Ship extends Controllable {
     }
     
     // Methods for managing score and health
+    
     public void addScore(int points) {
         score += points;
     }
