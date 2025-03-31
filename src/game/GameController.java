1eff248881d511d2a563523d851aaaf72d8ee931
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-03-31 22:45:26 +1000

    Update GameController.java

--- src/game/GameController.java
+++ src/game/GameController.java
@@ -129,7 +129,7 @@ public class GameController {
     public void pauseGame() {
         ui.pause();
         // 修改为不带句号的 "Game paused"
-        ui.log("Game paused");
+        ui.log("Game paused.");
     }
 
     /**
