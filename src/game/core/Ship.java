fcc7eb219eef6b98f94782d2c462156719273ecb
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-03-31 12:31:24 +1000

    Update Ship.java

--- src/game/core/Ship.java
+++ src/game/core/Ship.java
@@ -8,20 +8,20 @@ import game.GameModel;
 /**
  * Represents the player's ship.
  */
-public class Ship extends Controllable {
+public class Ship extends ObjectWithPosition implements Controllable {
     private int health;
     private int score;
     
     /**
-     * Constructs a Ship with the specified coordinates and initial health.
+     * Constructs a Ship with the specified coordinates.
+     * The ship's initial health is set to 100 and score is 0.
      *
      * @param x the x-coordinate of the ship.
      * @param y the y-coordinate of the ship.
-     * @param health the initial health of the ship.
      */
-    public Ship(int x, int y, int health) {
+    public Ship(int x, int y) {
         super(x, y);
-        this.health = health;
+        this.health = 100;
         this.score = 0;
     }
     
@@ -39,7 +39,7 @@ public class Ship extends Controllable {
     public void move(Direction direction) throws BoundaryExceededException {
         int newX = x;
         int newY = y;
-        switch (direction) {
+        switch(direction) {
             case UP:
                 newY = y - 1;
                 break;
@@ -53,7 +53,7 @@ public class Ship extends Controllable {
                 newX = x + 1;
                 break;
         }
-        if(newX < 0 || newX >= GameModel.GAME_WIDTH || newY < 0 || newY >= GameModel.GAME_HEIGHT) {
+        if (newX < 0 || newX >= GameModel.GAME_WIDTH || newY < 0 || newY >= GameModel.GAME_HEIGHT) {
             throw new BoundaryExceededException("Movement out of boundary: (" + newX + ", " + newY + ")");
         }
         x = newX;
@@ -61,6 +61,7 @@ public class Ship extends Controllable {
     }
     
     // Methods for managing score and health
+    
     public void addScore(int points) {
         score += points;
     }
@@ -84,4 +85,3 @@ public class Ship extends Controllable {
         return health;
     }
 }
-
