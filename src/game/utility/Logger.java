d93bf2248d8ee1450dcb85ada2a81e5d7ce10b51
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-04-01 11:28:26 +1000

    Update Logger.java

--- src/game/utility/Logger.java
+++ src/game/utility/Logger.java
@@ -1,12 +1,11 @@
 package game.utility;
 
 /**
- * A simple logger implementation that writes log messages to the standard output.
+ * A simple logger implementation that writes log messages to standard output.
  */
 public class ConsoleLogger implements Logger {
     @Override
     public void log(String message) {
-        // 直接输出传入的消息，不做额外格式化
         System.out.println(message);
     }
 }
