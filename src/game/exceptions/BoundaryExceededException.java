6ddb3efe698e94292b5f4544d9babb1e76938762
wuzhe1234567 <s4909464@student.uq.edu.au>
2025-04-02 10:04:02 +1000

    Update BoundaryExceededException.java

--- src/game/exceptions/BoundaryExceededException.java
+++ src/game/exceptions/BoundaryExceededException.java
@@ -4,6 +4,11 @@ package game.exceptions;
  * Exception thrown when a move exceeds the game boundaries.
  */
 public class BoundaryExceededException extends Exception {
+    /**
+     * Constructs a new BoundaryExceededException with the specified detail message.
+     *
+     * @param message the detail message.
+     */
     public BoundaryExceededException(String message) {
         super(message);
     }
